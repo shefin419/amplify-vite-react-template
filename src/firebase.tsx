e8a6646c111ce5b4b29361a8fa59e398ba02b1a5
Shefin Rasool <shass1475@gmail.com>
2025-08-12 12:44:56 +0530

    L commit by S

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -7,17 +7,18 @@ import {
 } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzL5FjaYD2jgYCB_USv_K8qtVGHqQEya0",
-  authDomain: "push-notify-53b8c.firebaseapp.com",
-  projectId: "push-notify-53b8c",
-  storageBucket: "push-notify-53b8c.appspot.com",
-  messagingSenderId: "625713046754",
-  appId: "1:625713046754:web:6c00bbd6b0846d34f2268e"
+  apiKey: "AIzaSyDHccvEes3cl5ju4z_mQzjpXLBDqonfqkw",
+  authDomain: "fb-facereko-notify.firebaseapp.com",
+  projectId: "fb-facereko-notify",
+  storageBucket: "fb-facereko-notify.firebasestorage.app",
+  messagingSenderId: "899924612948",
+  appId: "1:899924612948:web:55c6a9fb4945f37b56dac1",
+  measurementId: "G-5WQ42Q2XCQ"
 };
 
 const app = initializeApp(firebaseConfig);
 
-// ✅ Only get messaging if the environment supports it
+//✅ Only get messaging if the environment supports it
 function isMessagingSupported() {
   return (
     typeof window !== 'undefined' &&
@@ -45,11 +46,11 @@ export const requestForToken = () => {
   }
 
   return getToken(messaging, {
-    vapidKey: 'BMuk1RB7NmKW0MbFaD_vr1gWGJvFq5Dpa7XVVOqa_mjc8ALL4Pj9v8uXsiO4dGMfnSzYd72HZIwQCi5zLYVDTfU'
+    vapidKey: 'BO8C9aGkN9RkQAoZMz1Hl5lt6ElsQP6epyBIE6VuRiHRAfzhPNPA4Pd5_w5B90DMUowX3qh7bZCH7g2LvD_5CUE'
   })
     .then((currentToken) => {
       if (currentToken) {
-        // console.log('FCM Token:', currentToken);
+        console.log('FCM Token:', currentToken);
         localStorage.setItem('fcmToken', currentToken);
       } else {
         console.warn('No registration token available.');
@@ -58,7 +59,7 @@ export const requestForToken = () => {
     })
     .catch((err) => {
       console.error('An error occurred while retrieving token.', err);
-    });
+    } );
 };
 
 export const onMessageListener = () => {
